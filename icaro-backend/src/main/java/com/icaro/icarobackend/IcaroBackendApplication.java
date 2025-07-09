52d21cc9f6db5cccb906e663e863c66fd90eb104
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2025-07-09 17:57:48 +0200

    First commit

---
+++ icaro-backend/src/main/java/com/icaro/icarobackend/IcaroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.icaro.icarobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcaroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcaroBackendApplication.class, args);
+    }
+
+}
---
+++ icaro-backend/src/test/java/com/icaro/icarobackend/IcaroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icaro.icarobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcaroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
